fe666e1f194e097756a63b151619d6b080ea3134
Johannes Günther <103576477+johgyy@users.noreply.github.com>
2022-08-02 15:40:16 +0200

    Update sign-up.js

--- sign-up.js
+++ sign-up.js
@@ -4,13 +4,13 @@ function SignUp() {
     let lastName = document.getElementById('signup-last-name').value;
     let password = document.getElementById('sign-up-password').value;
     
-    team.push(`
+    team.push({
         'ID': '${team.length + 1}',
         'first-name': '${firstName}',
         'last-name': '${lastName}',
         'e-mail': '${email}',
         'src': './img/avatar${team.length + 1}.jpg',
-        'password': '${password}'`
+        'password': '${password}'}
     );
 }
 
