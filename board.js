a7f5c37aa49675c316bed3ed55328488c427e496
Johannes Günther <103576477+johgyy@users.noreply.github.com>
2022-08-19 21:33:24 +0200

    drag and drop

--- board.js
+++ board.js
@@ -1,10 +1,21 @@
-async function renderTasksToBoard() {
+
+async function initBoard() {
     await initBoardDB();
+    renderTasksToBoard();
+}
+
+async function renderTasksToBoard() {
+    document.getElementById('to-do').innerHTML = '';
+    document.getElementById('in-progress').innerHTML = '';
+    document.getElementById('testing').innerHTML = '';
+    document.getElementById('done').innerHTML = '';
+
     for (let i = 0; i < boardTasks.length; i++) {
         const task = boardTasks[i];
-        if (task['status'] = 'todo') {
+        // to-do column
+        if (task['status'] == 'todo') {
             document.getElementById('to-do').innerHTML += /*html*/ `
-            <div dragable="true" ondragover="startDragging(${i})" id="task-${i}" class="task-container">
+            <div draggable="true" ondragstart="startDragging(${i})" id="task-${i}" class="task-container">
                 <h3><b>${task['title']}</b></h3>
                 <div>${task['category']}</div>
                 <div class="task-container-subheadline">
@@ -16,9 +27,42 @@ async function renderTasksToBoard() {
             </div>
             `;
         }
+        // in-progress column
+        if (task['status'] == 'in-progress') {
+            document.getElementById('in-progress').innerHTML += /*html*/ `
+            <div draggable="true" ondragstart="startDragging(${i})" id="task-${i}" class="task-container">
+                <h3><b>${task['title']}</b></h3>
+                <div>${task['category']}</div>
+                <div class="task-container-subheadline">
+                    <span>${new Date(task['createdAt']).toLocaleDateString('eu-DE')}</span>
+                    <span id="urgency-${i}"><b>Urgency: </b></span>
+                </div>
+                <div id="assigned-to-${i}"><b>Assigned to: </b></div>
+                <div class="task-description"><b>Description:</b><br> ${task['description']}</div>
+            </div>
+            `;
+        }
+        // testing column
+        if (task['status'] == 'testing') {
+            document.getElementById('testing').innerHTML += /*html*/ `
+                    <div draggable="true" ondragstart="startDragging(${i})" id="task-${i}" class="task-container">
+                        <h3><b>${task['title']}</b></h3>
+                        <div>${task['category']}</div>
+                        <div class="task-container-subheadline">
+                            <span>${new Date(task['createdAt']).toLocaleDateString('eu-DE')}</span>
+                            <span id="urgency-${i}"><b>Urgency: </b></span>
+                        </div>
+                        <div id="assigned-to-${i}"><b>Assigned to: </b></div>
+                        <div class="task-description"><b>Description:</b><br> ${task['description']}</div>
+                    </div>
+                    `;
+        }
+
+
         // render assigned members
         for (let j = 0; j < task['assignedMember'].length; j++) {
             const assignedMember = task['assignedMember'][j];
+            document.getElementById('assigned-to-' + i).innerHTML = '';
             document.getElementById('assigned-to-' + i).innerHTML += `<br>${assignedMember['firstName']} ${assignedMember['lastName']}`
         }
         // render urgency
@@ -39,7 +83,7 @@ async function renderTasksToBoard() {
 
 let currentDraggedElement;
 
-function startDragging(id){
+function startDragging(id) {
     currentDraggedElement = id;
 }
 
@@ -49,8 +93,16 @@ function allowDrop(ev) {
 
 function moveTo(category) {
     allTasks[currentDraggedElement]['status'] = category;
+    renderTasksToBoard();
 }
 
+function highlight(category) {
+    document.getElementById(category).classList.add('highlight');
+}
+
+function removeHighlight(category) {
+    document.getElementById(category).classList.remove('highlight');
+}
 
 // renderToDo();
 // renderInProgress();
