d9b2acc261640c6b33b516e2baef64cfa82e30e6
Johannes Günther <103576477+johgyy@users.noreply.github.com>
2022-08-19 21:39:45 +0200

    add task statusbacklog

--- addTask.js
+++ addTask.js
@@ -24,7 +24,7 @@ async function addTask() {
             "urgency": urgency.value,
             "description": description.value,
             "createdAt": new Date().getTime(),
-            "status": "todo",
+            "status": "backlog",
             "lastEdit": ""
         };
         //add object to array and morph it to text to save it in locaStorage
